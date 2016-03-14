537d44b1e61fa71a8c49906431c94ca8c582fbed
AnastasiaSulyagina <anastasia.sulyagina@gmail.com>
2016-03-14 13:35:35 +0300

    initial

---
+++ app/src/main/java/com/csc/lesson3/SearchActivity.java
@@ -0,0 +1,88 @@
+package com.csc.lesson3;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.TextView;
+import com.csc.lesson3.api.ImageSearch;
+import com.csc.lesson3.api.Translator;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SearchActivity extends AppCompatActivity {
+
+    ArrayList<Bitmap> images;
+    TextView textView;
+    private GridView gridView;
+    private GridViewAdapter gridAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.search_activity);
+        EditText word = (EditText) findViewById(R.id.editText);
+        gridView = (GridView) findViewById(R.id.gridview);
+        textView = (TextView) findViewById(R.id.textView);
+
+        String w = getIntent().getStringExtra(MainActivity.WORD);
+
+        word.setText(w);
+        translate(w);
+        getImages(w);
+
+        gridAdapter = new GridViewAdapter(this, R.layout.grid_item_layout, images);
+        gridView.setAdapter(gridAdapter);
+    }
+
+    private void translate(String word) {
+        new TranslateAsyncTask().execute(word);
+    }
+
+    private class TranslateAsyncTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            textView.setText(s);
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Translator t = new Translator();
+            return t.translate(strings[0]);
+        }
+    }
+
+    private void getImages(String word) {
+        new GetImagesAsyncTask().execute(word);
+    }
+
+    private class GetImagesAsyncTask extends AsyncTask<String, Void, List<Bitmap> > {
+        @Override
+        protected void onPostExecute(List<Bitmap> res) {
+            super.onPostExecute(res);
+            images.addAll(res);
+        }
+
+        @Override
+        protected List<Bitmap> doInBackground(String... strings) {
+            ImageSearch s = new ImageSearch();
+            List<String> result = s.getImages(strings[0]);
+            List<Bitmap> images = new ArrayList<Bitmap>();
+            try{
+                for(String path: result) {
+                    URL url = new URL(path);
+                    Bitmap img = BitmapFactory.decodeStream(url.openStream());
+                }
+            } catch (Exception e) {}
+            return images;
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/csc/lesson3/api/ImageSearch.java
@@ -0,0 +1,65 @@
+package com.csc.lesson3.api;
+
+import android.util.Base64;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by anastasia on 14.03.16.
+ */
+public class ImageSearch {
+    private static final String KEY = "3SFImejWQhyXfHDeHQ8tguQCWBCQlDpJdXrkmoSlo0c";
+    private static final String HOME_URL = "https://api.datamarket.azure.com/Bing/Search/v1/Image";
+    private static final String ENC = "UTF-8";
+    private static final int CNT = 20;
+
+
+    public List<String> getImages(String word) {
+        try {
+            URL url = new URL(HOME_URL + "?Query='" + URLEncoder.encode(word) + "'" + "&$format=json&$top=" + CNT);
+            HttpURLConnection httpConnection = (HttpURLConnection) url.openConnection();
+            httpConnection.setRequestMethod("GET");
+            httpConnection.setRequestProperty("Content-Type","text/plain; charset=" + ENC);
+            httpConnection.setRequestProperty("Accept-Charset", ENC);
+            httpConnection.setRequestProperty("Authorization", "Basic " + Base64.encodeToString((":" + KEY).getBytes(), Base64.DEFAULT));
+
+            httpConnection.connect();
+
+            String line;
+            BufferedReader buffReader = new BufferedReader(new InputStreamReader(httpConnection.getInputStream()));
+            StringBuilder strBuilder = new StringBuilder();
+            while ((line = buffReader.readLine()) != null) {
+                strBuilder.append(line + '\n');
+            }
+            List<String> images = parseImagesFromJSON(strBuilder.toString());
+            return images;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+    public static List<String> parseImagesFromJSON(String str) {
+        try {
+            JSONObject object = new JSONObject(str);
+            JSONArray array = (JSONArray) object.get("text");
+            List<String> images = new ArrayList<String>();
+
+            for (int i = 0; i < array.length(); ++i) {
+                images.add(array.get(i).toString());
+            }
+
+            return images;
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
+}
